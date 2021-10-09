c2e5d823304c177b739f175df612b27d6411c1ce
lmatola <lazarofmatola@gmail.com>
2021-10-09 21:34:57 +0200

    up

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -68,7 +68,7 @@ class Director:
             self (Director): An instance of Director.
         """
         
-        Next_card = input(f"Next card was: {self.dealer.result2}")        
+        print(f"Next card was: {self.dealer.result2}")        
         print(f"Your score is: {self.score}")
         if self.score <= 0:
             self.dealer.score_list.clear()
